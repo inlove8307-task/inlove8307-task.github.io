9855975156e75d4664ecfe424e64928f1acb7769
inlove8307-task <inlove8307.task@gmail.com>
2019-06-14 08:53:28 +0000

    set data filter style

--- dev/src/components/DataFilter.vue
+++ dev/src/components/DataFilter.vue
@@ -1,23 +1,34 @@
 <template>
-  <div>
-    <span class="filter" v-show="!disabled">
-      <label for="FILTER_TAG">구분</label>
+  <div class="filter">
+    <span class="filter-select" v-show="!disabled">
+      <button @click="tagList = !tagList"><span>{{ filter.tag }}</span><i class="material-icons">keyboard_arrow_down</i></button>
+      <ul v-show="tagList">
+        <li
+          v-for="item in tag"
+          v-bind:key="item.key">
+          <button @click="filter.tag = item; tagList = false">{{ item }}</button>
+        </li>
+      </ul>
+      <!--
       <select id="FILTER_TAG" v-model="filter.tag">
         <option value="">전체</option>
         <option v-for="item in tag" v-bind:key="item.key">{{ item }}</option>
       </select>
+      -->
     </span>
-    <span class="filter" v-show="!disabled">
+    <span class="filter-input" v-show="!disabled">
       <label for="FILTER_DATE">날짜</label>
       <input type="number" id="FILTER_DATE" v-model="filter.date" @keyup.enter="getData">
     </span>
-    <span class="filter" v-show="!disabled">
+    <span class="filter-input" v-show="!disabled">
       <label for="FILTER_TITLE">제목</label>
       <input type="text" id="FILTER_TITLE" v-model="filter.title" @keyup.enter="getData">
     </span>
-    <button @click="getData" v-show="!disabled"><i class="material-icons">search</i></button>
-    <button @click="clear" v-show="!disabled"><i class="material-icons">clear</i></button>
-    <span class="menu" v-show="code != 'C02'">
+    <span class="filter-button" v-show="!disabled">
+      <button @click="getData"><i class="material-icons">search</i></button>
+      <button @click="clear"><i class="material-icons">clear</i></button>
+    </span>
+    <span class="filter-button filter-button-right" v-show="code != 'C02'">
       <button @click="setView('days')"><i class="material-icons">view_list</i></button>
       <button @click="setView('week')"><i class="material-icons">view_column</i></button>
     </span>
@@ -32,10 +43,11 @@ export default {
     return {
       filter: {
         code : this.code,
-        tag: '',
+        tag: '태그',
         date: null,
         title: null
-      }
+      },
+      tagList: false
     }
   },
   computed: {
@@ -77,121 +89,144 @@ export default {
 <style lang="scss" scoped>
 @media (min-width: 1281px) {
   /* ##Device = Desktops ##Screen = 1281px to higher resolution desktops */
-  div {
-    box-sizing: border-box;
-    margin: 0 10px;
-    padding: 10px 0 0;
-    border: 0;
+  .filter {
+    display: flex;
+    justify-content: stretch;
+    align-items: stretch;
+    margin-top: 10px;
+    padding: 0 10px 10px 10px;
     border-bottom: 1px solid #ccc;
-    background-color: transparent;
 
-    &::after {
-      content: '';
-      display: block;
-      clear: both;
+    .filter-select {
+      // flex: 1;
+      position: relative;
+
+      button {
+        display: flex;
+        align-items: center;
+        position: relative;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 30px;
+        border: 1px solid #222;
+        border-radius: 3px;
+        background-color: #333;
+        box-shadow: inset 1px 1px 0 #444;
+        font-size: 12px;
+        color: #fff;
+        cursor: pointer;
+
+        span {
+          flex: 1;
+          padding-left: 5px;
+          text-align: left;
+          color: #fff;
+        }
+      }
+
+      ul {
+        position: absolute;
+        bottom: 0;
+        top: 29px;
+        z-index: 1;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        border-bottom-left-radius: 3px;
+        border-bottom-right-radius: 3px;
+
+        li {
+          margin-top: -1px;
+          border: 1px solid #222;
+          background-color: #333;
+          box-shadow: inset 1px 1px 0 #444;
+
+          &:last-child {
+            border-bottom-left-radius: 3px;
+            border-bottom-right-radius: 3px;
+          }
+
+          button {
+            margin: 0;
+            padding: 0 0 0 5px;
+            width: 100%;
+            height: 30px;
+            border: 0;
+            background-color: transparent;
+          }
+        }
+      }
+    }
+    .filter-input {
+      // flex: 1;
+      padding-left: 5px;
+
+      label {
+        float: left;
+        box-sizing: border-box;
+        padding: 0 5px;
+        height: 30px;
+        border: 1px solid #222;
+        border-top-left-radius: 3px;
+        border-bottom-left-radius: 3px;
+        background-color: #333;
+        box-shadow: inset 1px 1px 0 #444;
+        font-size: 12px;
+        line-height: 30px;
+        color: #fff;
+      }
+      input {
+        float: left;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0 0 0 5px;
+        height: 30px;
+        border: 1px solid #222;
+        border-top-right-radius: 3px;
+        border-bottom-right-radius: 3px;
+        background-color: #333;
+        box-shadow: inset 1px 1px 3px #222;
+        font-size: 12px;
+        color: #fff;
+
+        &::-webkit-outer-spin-button,
+        &::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+        }
+      }
     }
 
-    .filter {
-      float: left;
-      margin: 0 5px 10px 0;
-
+    .filter-button {
       &::after {
         content: '';
         display: block;
         clear: both;
       }
 
-      label {
-        float: left;
-        padding: 5px;
-        border-top: 1px solid #333;
-        border-bottom: 1px solid #333;
-        border-left: 1px solid #333;
-        border-top-left-radius: 3px;
-        border-bottom-left-radius: 3px;
-        background-color: #444;
-        box-shadow: inset 1px 1px 0 #555;
-        font-family: 'Noto Sans KR', 'Malgun Gothic';
-        font-weight: 400;
-        font-size: 12px;
-        color: #fff;
-        cursor: pointer;
-      }
-
-      select {
-        float:left;
-        padding: 4px;
-        border: 1px solid #333;
-        border-top-right-radius: 3px;
-        border-bottom-right-radius: 3px;
-        background-color: #444;
-        box-shadow: inset 1px 1px 0 #555;
-        font-family: 'Noto Sans KR', 'Malgun Gothic';
-        font-weight: 400;
-        font-size: 12px;
-        color: #fff;
-        outline: none;
-        cursor: pointer;
-
-        option {
-          font-family: 'Noto Sans KR', 'Malgun Gothic';
-          font-weight: 400;
-          font-size: 12px;
-        }
-      }
-
-      input {
-        float:left;
-        padding: 5px;
-        border: 1px solid #333;
-        border-top-right-radius: 3px;
-        border-bottom-right-radius: 3px;
-        background-color: #444;
-        box-shadow: inset 1px 1px 4px #333;
-        font-family: 'Noto Sans KR', 'Malgun Gothic';
-        font-weight: 400;
-        font-size: 12px;
-        color: #fff;
-        outline: none;
-
-        &[type=number]::-webkit-inner-spin-button,
-        &[type=number]::-webkit-outer-spin-button {
-          -webkit-appearance: none;
-          -moz-appearance: none;
-          appearance: none;
-        }
-      }
-    }
-
-    button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      float:left;
-      box-sizing: border-box;
-      margin-right:5px;
-      padding: 0;
-      width: 30px;
-      height: 30px;
-      border: 1px solid #333;
-      border-radius: 3px;
-      background-color: #444;
-      box-shadow: inset 1px 1px 0 #555;
-      font-family: 'Noto Sans KR', 'Malgun Gothic';
-      font-weight: 400;
-      font-size: 12px;
-      color: #fff;
-      cursor: pointer;
-    }
-
-    .menu {
-      float: right;
-      margin-bottom: 10px;
-
       button {
-        margin-right: 0;
-        margin-left: 5px;
+        float: left;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        margin: 0 0 0 5px;
+        padding: 0;
+        width: 30px;
+        height: 30px;
+        border: 1px solid #222;
+        border-radius: 3px;
+        background-color: #333;
+        box-shadow: inset 1px 1px 0 #444;
+        color: #fff;
+        cursor: pointer;
       }
+    }
+
+    .filter-right {
+      float: right;
     }
   }
 }
